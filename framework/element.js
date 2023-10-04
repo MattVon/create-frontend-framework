a1bc3f9c18930e6898952177b17d5f70b5f166ed
Matthew Foster <Matt.SGTC@gmail.com>
2023-10-04 22:32:50 +0100

    Added Snabbdom

---
+++ framework/element.js
@@ -0,0 +1,16 @@
+import { h } from 'snabbdom';
+
+const createElement = tagName => (strings, ...args) => ({
+  type: "element",
+  template: h(
+    tagName,
+    {},
+    strings.reduce(
+      (acc, currentString, index) => acc + currentString + (args[index] || ""),
+      ""
+    )
+  )
+});
+
+export const div = createElement("div");
+export const p = createElement("p");
---
+++ framework/index.js
@@ -0,0 +1,7 @@
+import * as snabbdom from "snabbdom";
+const patch = snabbdom.init([]);
+
+export const init = (selector, component) => {
+  const app = document.querySelector(selector);
+  patch(app, component.template);
+};
